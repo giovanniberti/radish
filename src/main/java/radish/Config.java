d53aae9e045e7736d2e9449fd7d7c377407c6565
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-03-20 16:15:12 +0100

    refactor(Config): make `Config` agnostic to local/locally distributed mode

--- src/main/java/radish/Config.java
+++ src/main/java/radish/Config.java
@@ -9,25 +9,42 @@ public class Config {
     public final String accessTokenSecret;
     public final String apiKey;
     public final String apiSecretKey;
+    private final boolean localMode;
 
-    public Config(String accessToken, String accessTokenSecret, String apiKey, String apiSecretKey) {
+    public Config(boolean localMode, String accessToken, String accessTokenSecret, String apiKey, String apiSecretKey) {
+        this.localMode = localMode;
         this.accessToken = accessToken;
         this.accessTokenSecret = accessTokenSecret;
         this.apiKey = apiKey;
         this.apiSecretKey = apiSecretKey;
     }
 
-    public static Config getInstance() {
+    public Config(String accessToken, String accessTokenSecret, String apiKey, String apiSecretKey) {
+        this(true, accessToken, accessTokenSecret, apiKey, apiSecretKey);
+    }
+
+    public static Config getInstance(boolean localMode) {
         if (Config.INSTANCE == null) {
-            Dotenv dotenv = Dotenv.configure().directory("/").load();
+            Dotenv dotenv;
+
+            if (localMode) {
+                dotenv = Dotenv.configure().load();
+            } else {
+                dotenv = Dotenv.configure().directory("/").load();
+            }
+
             String accessToken = dotenv.get("ACCESS_TOKEN");
             String accessTokenSecret = dotenv.get("ACCESS_TOKEN_SECRET");
             String apiKey = dotenv.get("API_KEY");
             String apiSecretKey = dotenv.get("API_SECRET_KEY");
 
-            Config.INSTANCE = new Config(accessToken, accessTokenSecret, apiKey, apiSecretKey);
+            Config.INSTANCE = new Config(localMode, accessToken, accessTokenSecret, apiKey, apiSecretKey);
         }
 
         return INSTANCE;
+    }
+
+    public static Config getInstance() {
+        return getInstance(true);
     }
 }
