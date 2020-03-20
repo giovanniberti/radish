c59db795456002b61688b1e05470467c7a3e48cf
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-03-20 16:22:43 +0100

    refactor(TwitterSpout): refactor `TwitterSpout` to use twitter4j library

--- src/main/java/radish/speed/TwitterSpout.java
+++ src/main/java/radish/speed/TwitterSpout.java
@@ -1,69 +1,68 @@
 package radish.speed;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.storm.spout.SpoutOutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.base.BaseRichSpout;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
-import winterwell.jtwitter.AStream;
-import winterwell.jtwitter.Twitter;
-import winterwell.jtwitter.TwitterEvent;
-import winterwell.jtwitter.TwitterStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import radish.Config;
+import twitter4j.ExtendedMediaEntity;
+import twitter4j.MediaEntity;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.auth.AccessToken;
 
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class TwitterSpout extends BaseRichSpout {
-    private static final Logger logger = LogManager.getLogger(TwitterSpout.class);
+    private static final Logger logger = LoggerFactory.getLogger(TwitterSpout.class);
+    private final String keyword;
+    private final AccessToken accessToken;
 
     private SpoutOutputCollector collector;
-    private String keyword;
-    private List<String> urlsToProcess = new ArrayList<>();
+    private LinkedBlockingQueue<String> urlsToProcess;
+    private TwitterStream stream;
 
-    public TwitterSpout(String keyword, TwitterStream stream) {
+    public TwitterSpout(String keyword, AccessToken accessToken) {
         this.keyword = keyword;
-
-        List<String> keywordList = new ArrayList<>();
-        keywordList.add(keyword);
-        stream.setTrackKeywords(keywordList);
-
-        stream.addListener(new AStream.IListen() {
-            @Override
-            public boolean processEvent(TwitterEvent twitterEvent) throws Exception {
-                return true;
-            }
-
-            @Override
-            public boolean processSystemEvent(Object[] objects) throws Exception {
-                return true;
-            }
-
-            @Override
-            public boolean processTweet(Twitter.ITweet tweet) throws Exception {
-
-                List<Twitter.TweetEntity> tweetMedias = tweet.getTweetEntities(Twitter.KEntityType.media);
-
-                List<String> mediaUrls = tweetMedias
-                        .stream()
-                        .map(Twitter.TweetEntity::mediaUrl)
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toList());
-
-                logger.debug("Received " + mediaUrls.size() + " new tweets. Adding them to spout.");
-
-                urlsToProcess.addAll(mediaUrls);
-
-                return true;
-            }
-        });
+        this.accessToken = accessToken;
     }
 
     @Override
     public void open(Map<String, Object> conf, TopologyContext context, SpoutOutputCollector collector) {
         this.collector = collector;
+        urlsToProcess = new LinkedBlockingQueue<>();
+
+        Config radishConf = Config.getInstance();
+
+        stream = TwitterStreamFactory.getSingleton();
+        stream.setOAuthConsumer(radishConf.apiKey, radishConf.apiSecretKey);
+        stream.setOAuthAccessToken(accessToken);
+
+        stream.onStatus(status -> {
+            logger.info("### RECEIVED STATUS");
+
+            for (ExtendedMediaEntity extendedMediaEntity : status.getExtendedMediaEntities()) {
+                logger.debug("Found extended entity with type " + extendedMediaEntity.getType());
+                if (extendedMediaEntity.getType().equals("photo")) {
+                    urlsToProcess.add(extendedMediaEntity.getMediaURL());
+                }
+            }
+
+            for (MediaEntity mediaEntity : status.getMediaEntities()) {
+                logger.debug("Found entity with type " + mediaEntity.getType());
+                if (mediaEntity.getType().equals("photo")) {
+                    urlsToProcess.add(mediaEntity.getMediaURL());
+                }
+            }
+
+            logger.debug("Current urls: " + urlsToProcess);
+        });
+        stream.filter(keyword);
     }
 
     @Override
@@ -73,12 +72,17 @@ public class TwitterSpout extends BaseRichSpout {
 
     @Override
     public void nextTuple() {
-        logger.debug("emitting most recent received tweets");
         for (Iterator<String> iterator = urlsToProcess.iterator(); iterator.hasNext(); ) {
             String url = iterator.next();
+            logger.info("~~~ EMITTING tuple: " + keyword + " " + url);
             collector.emit(new Values(keyword, url));
 
             iterator.remove();
         }
+    }
+
+    @Override
+    public void close() {
+        stream.shutdown();
     }
 }
