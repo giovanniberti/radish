7f46aafb8fb592192f4be7ac392605efcc06581a
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-04-13 15:21:31 +0200

    refactor(Radish): change output table and input table name

--- src/main/java/radish/batch/Radish.java
+++ src/main/java/radish/batch/Radish.java
@@ -11,7 +11,8 @@ import org.apache.hadoop.util.ToolRunner;
 import radish.HBaseSchema;
 
 public class Radish extends Configured implements Tool {
-    private static final String RADISH_TABLE = "radish";
+    private static final String IMAGES_TABLE = "images";
+    public static final String CLUSTERS_TABLE = "clusters";
 
     @Override
     public int run(String[] args) throws Exception {
@@ -23,7 +24,7 @@ public class Radish extends Configured implements Tool {
         Scan scan = new Scan();
         scan.addFamily(HBaseSchema.DATA_COLUMN_FAMILY);
         TableMapReduceUtil.initTableMapperJob(
-                RADISH_TABLE,
+                IMAGES_TABLE,
                 scan,
                 ImageDBMapper.class,
                 Text.class,
@@ -31,7 +32,7 @@ public class Radish extends Configured implements Tool {
                 job
         );
 
-        TableMapReduceUtil.initTableReducerJob(RADISH_TABLE, KeywordReducer.class, job);
+        TableMapReduceUtil.initTableReducerJob(CLUSTERS_TABLE, KeywordReducer.class, job);
 
         boolean success = job.waitForCompletion(true);
         return success ? 0 : 1;
