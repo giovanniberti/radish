6aff3f1acb7e59c7bb639b942576a5dbdb3a70e8
Giovanni Berti <dev.giovanniberti@gmail.com>
2020-04-13 15:25:23 +0200

    refactor(FeatureMapperBolt): add keyword field to output tuple

--- src/main/java/radish/speed/FeatureMapperBolt.java
+++ src/main/java/radish/speed/FeatureMapperBolt.java
@@ -15,8 +15,9 @@ import java.util.Base64;
 import java.util.Map;
 
 public class FeatureMapperBolt extends BaseRichBolt {
-    public static final String ID = "id";
-    public static final String FEATURES = "features";
+    public static final String ID = FeatureBolt.ID;
+    public static final String FEATURES = FeatureBolt.FEATURES;
+    public static final String KEYWORD = FeatureBolt.KEYWORD;
 
     private static final Logger logger = LoggerFactory.getLogger(FeatureMapperBolt.class);
     private OutputCollector collector;
@@ -28,18 +29,21 @@ public class FeatureMapperBolt extends BaseRichBolt {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields(ID, FEATURES));
+        declarer.declare(new Fields(ID, KEYWORD, FEATURES));
     }
 
     @Override
     public void execute(Tuple input) {
         String id = input.getStringByField(FeatureBolt.ID);
+        String keyword = input.getStringByField(FeatureBolt.KEYWORD);
         double[] featureVector = (double[]) input.getValueByField(FeatureBolt.FEATURES);
+
+        logger.info("Persisting features: "  + featureVector);
 
         Base64.Encoder encoder = Base64.getEncoder();
         String encodedFeatureVector = new String(encoder.encode(HBaseUtils.doubleArrayToBytes(featureVector)));
 
-        collector.emit(input, new Values(id, encodedFeatureVector));
+        collector.emit(input, new Values(id, keyword, encodedFeatureVector));
         collector.ack(input);
     }
 }
